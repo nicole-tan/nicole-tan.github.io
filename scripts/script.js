892780bddae00681c56a93e6b71f99f4add38643
Nicole Tan <nat34@cornell.edu>
2017-05-29 18:58:57 -0400

    Fixed font problem

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+//only called for the class card
+function cardHover(id) { 
+	//darkens the background 
+	//checks id and spits out appropriate response
+	if (id == "felongeist_1") {
+		document.getElementById("felongeist_1").style.backgroundColor = rgb(255,255,255);
+	}
+
+}
